74511413e41a92c9ee7d12184721d6a2c24d3844
rokie <motiurga@gmail.com>
2017-10-10 10:00:49 -0700

    Worked on NITIKA.java

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TeachingKidsProgramming
+#The Format Is
+#https://www.codechef.com/problems/'x'
+#'x' is the name of the .java file
--- src/NITIKA.java
+++ src/NITIKA.java
@@ -10,26 +10,33 @@ public class NITIKA
     String output;
     char capitalize;
     int letter;
-    int numOfWords;
-    String word;
+    int numOfWords = 0;
+    String word = "";
     String[] words = new String[100];
-    for (int i = 0; i < testcases; i++)
+    for (int i = 0; i < testcases + 1; i++)
     {
       //clear variables & ask for input
       words = new String[words.length];
-      rawInput = scan.next();
-      //split words at spaces
+      rawInput = scan.nextLine();
+      numOfWords = 0;
+      //split words at spaces      
       for (int g = 0; g < rawInput.length(); g++)
       {
-        if (rawInput.charAt(g) == ' ')
+        if (rawInput.charAt(g) == ' ' || g == rawInput.length())
         {
           words[numOfWords] = word;
+          word = "";
           numOfWords++;
+        }
+        else
+        {
+          word = word + rawInput.charAt(g);
         }
       }
       //capitalize each word
       //delete unnecessary parts(anything after capital)
       //put back together again and print it
     }
+    //close and reset some variables
   }
 }
