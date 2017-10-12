f78879e89c08c6dddf6f03ea4215f35753f2a4e2
rokie <motiurga@gmail.com>
2017-10-12 14:32:23 -0700

    Worked on NITIKA.java

--- src/NITIKA.java
+++ src/NITIKA.java
@@ -6,23 +6,24 @@ public class NITIKA
   {
     Scanner scan = new Scanner(System.in);
     int testcases = scan.nextInt();
-    String rawInput;
-    String output;
-    char capitalize;
-    int letter;
+    String rawInput = "";
+    String output = "";
+    char capitalize = ' ';
+    String letter = "";
     int numOfWords = 0;
     String word = "";
     String[] words = new String[100];
-    for (int i = 0; i < testcases + 1; i++)
+    for (int i = 0; i < testcases; i++)
     {
       //clear variables & ask for input
       words = new String[words.length];
-      rawInput = scan.nextLine();
+      rawInput = scan.nextLine() + " ";
       numOfWords = 0;
+      word = "";
       //split words at spaces      
       for (int g = 0; g < rawInput.length(); g++)
       {
-        if (rawInput.charAt(g) == ' ' || g == rawInput.length())
+        if (rawInput.charAt(g) == ' ' || g == rawInput.length() + 1)
         {
           words[numOfWords] = word;
           word = "";
@@ -33,10 +34,15 @@ public class NITIKA
           word = word + rawInput.charAt(g);
         }
       }
+      numOfWords--;
+      System.out.println("words[0] is " + words[0]);
+      System.out.println("words[1] is " + words[1]);
+      System.out.println("words[2] is " + words[2]);
+      System.out.println("numOfWord is " + numOfWords);
       //capitalize each word
       //delete unnecessary parts(anything after capital)
       //put back together again and print it
+      //close and reset some variables
     }
-    //close and reset some variables
   }
 }
