1730446e1d6290f1edc7b1020a637c1ecc49f2a9
rokie <motiurga@gmail.com>
2017-10-16 10:52:59 -0700

    Finished NITIKA.java

--- src/NITIKA.java
+++ src/NITIKA.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class NITIKA
 {
   public static void main(String[] args)
@@ -14,25 +13,46 @@ public class NITIKA
     String rawInput = "";
     String[] words = new String[100];
     String changes = "";
-    
-    for(int i = 0; i < testcases; i++) {
-    	//reset all variables
-    	rawInput = scan.nextLine();
-    	
-    	//Split the words
-    	words = rawInput.split(" ");
-    	numOfWords = rawInput.split(" ").length;
-    	//System.out.println(rawInput.split(" ").length);
-    	
-    	//ready words for more processing
-    	for(int g = 0; g < numOfWords; g++) {
-    		changes = words[g];
-    		changes = changes.substring(0, 1).toUpperCase() + changes.substring(1);
-    		
-    		words[g] = changes;
-    		System.out.println(changes);
-    	}
-    	//process them
-    }    
+    String output = "";
+    String preout = "";
+    for (int i = 0; i < testcases; i++)
+    {
+      //reset all variables
+      rawInput = scan.nextLine();
+      output = "";
+      preout = "";
+      changes = "";
+      for (int r = 0; r < words.length; r++)
+      {
+        words[r] = "";
+      }
+      //Split the words
+      words = rawInput.split(" ");
+      numOfWords = rawInput.split(" ").length;
+      //ready words for more processing
+      for (int g = 0; g < numOfWords - 1; g++)
+      {
+        //Change to upper case
+        changes = words[g];
+        changes = changes.substring(0, 1).toUpperCase() + changes.substring(1);
+        //shortens to first character plus .
+        changes = changes.charAt(0) + ".";
+        words[g] = changes;
+      }
+      changes = words[numOfWords - 1];
+      changes = changes.substring(0, changes.length()).toLowerCase();
+      changes = changes.substring(0, 1).toUpperCase() + changes.substring(1);
+      words[numOfWords - 1] = changes;
+      // put the letters in words[] to one string
+      for (int o = 0; o < numOfWords; o++)
+      {
+        preout = preout + " " + words[o];
+      }
+      for (int o = 0; o < preout.length() - 1; o++)
+      {
+        output = output + preout.charAt(o + 1) + "";
+      }
+      System.out.println(output);
+    }
   }
 }
