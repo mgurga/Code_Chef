0199147be5df38c9113357a477941d6dda761114
rokie <motiurga@gmail.com>
2017-10-26 14:32:09 -0700

    started LONGSEQ.java

---
+++ src/LONGSEQ.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LONGSEQ
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    int testcases = scan.nextInt();
+    String input = "";
+    for (int i = 0; i < testcases; i++)
+    {
+      input = scan.next();
+      for()
+    }
+  }
+}
