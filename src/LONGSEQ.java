705d39f11b71f6034f1c11bc25c667ec11bd50cd
rokie <motiurga@gmail.com>
2017-10-30 10:57:04 -0700

    Redid LONGSEQ.java

--- src/LONGSEQ.java
+++ src/LONGSEQ.java
@@ -7,13 +7,49 @@ public class LONGSEQ
     Scanner scan = new Scanner(System.in);
     int testcases = scan.nextInt();
     String input = "";
+    boolean yeaOrNay = false;
     for (int i = 0; i < testcases; i++)
     {
       input = scan.next();
+      yeaOrNay = false;
       int[] numbers = new int[input.length()];
-      for (int g = 0; g < input.length(); g++)
+      int[] numbersToChange = new int[input.length()];
+      for (int f = 0; f < input.length(); f++)
       {
-        numbers[g] = input.charAt(g);
+        numbers[f] = input.charAt(f) - '0';
+      }
+      numbersToChange = numbers;
+      for (int j = 0; j < input.length() - 1; j++)
+      {
+        if (numbersToChange[j] == 1)
+        {
+          numbersToChange[j] = 0;
+        }
+        else if (numbersToChange[j] == 0)
+        {
+          numbersToChange[j] = 1;
+        }
+        int count = 0;
+        for (int g = 0; g < numbers.length; g++)
+        {
+          if (numbersToChange[g] == 1)
+          {
+            count++;
+          }
+        }
+        if (count == input.length() || count == 0)
+        {
+          yeaOrNay = true;
+        }
+        numbersToChange = numbers;
+      }
+      if (yeaOrNay == true)
+      {
+        System.out.println("Yes");
+      }
+      else
+      {
+        System.out.println("No");
       }
     }
   }
