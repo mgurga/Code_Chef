d6a52a2a03b92fc3936d8e84f763589909e91555
rokie95 <motiurga@gmail.com>
2018-03-10 11:56:16 -0800

    Started CANDY123

---
+++ src/CANDY123.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in);
+        int testcases = scan.nextInt();
+        scan.nextLine();
+        String input = "";
+        for (int i = 0; i < testcases; i++)
+        {
+            input = scan.nextLine();
+            int curNumOfCandies = 1;
+            int limakCandies = 0;
+            int bobCandies = 0;
+            String[] bobAndLimak = input.split(" ");
+            int limakLimit = Integer.parseInt(bobAndLimak[0]);
+            int bobLimit = Integer.parseInt(bobAndLimak[1]);
+            
+        }
+	}
+}
